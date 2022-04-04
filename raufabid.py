500d39a22b411c1fd7b637a59e6c68c35bab6028
abidgurbanov0 <102621027+abidgurbanov0@users.noreply.github.com>
2022-04-04 19:52:43 +0400

    Update raufabid.py

--- raufabid.py
+++ raufabid.py
@@ -4,7 +4,6 @@ import pygame
 import random 
 import math
 
-from sympy import true
     
 pygame.init()
 pygame.display.set_caption("Abid Amculleey rauf")
@@ -46,25 +45,56 @@ class gameframe():
             entity.y = SNAKE_SIZE
         if(entity.y < 0):
             entity.y = SCREEN_HEIGHT - SNAKE_SIZE
-def getKey():
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    return KEY["UP"]
-                elif event.key == pygame.K_DOWN:
-                    return KEY["DOWN"]
-                elif event.key == pygame.K_LEFT:
-                    return KEY["LEFT"]
-                elif event.key == pygame.K_RIGHT:
-                    return KEY["RIGHT"]
-                elif event.key == pygame.K_ESCAPE:
-                    return "exit"
-                elif event.key == pygame.K_y:
-                    return "yes"
-                elif event.key == pygame.K_n:
-                    return "no"
-            if event.type == pygame.QUIT:
-                sys.exit() 
+    def getKey():
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        return KEY["UP"]
+                    elif event.key == pygame.K_DOWN:
+                        return KEY["DOWN"]
+                    elif event.key == pygame.K_LEFT:
+                        return KEY["LEFT"]
+                    elif event.key == pygame.K_RIGHT:
+                        return KEY["RIGHT"]
+                    elif event.key == pygame.K_ESCAPE:
+                        return "exit"
+                    elif event.key == pygame.K_y:
+                        return "yes"
+                    elif event.key == pygame.K_n:
+                        return "no"
+                if event.type == pygame.QUIT:
+                    sys.exit() 
+    def endGame():
+        message = game_over_font.render("Oyundan sikdirdin",1,pygame.Color("blue"))
+        message_play_again = play_again_font.render("Oynuyax? Y/N",1,pygame.Color("red"))
+        screen.blit(message,(320,240))
+        screen.blit(message_play_again,(320+12,240+40))
 
+        pygame.display.flip()
+        pygame.display.update()
+        
+        myKey = getKey()
+        while(myKey != "exit"):
+            if(myKey == "yes"):
+                main()
+            elif(myKey == "no"):
+                break
+            myKey = getKey()
+            gameClock.tick(FPS)
+        sys.exit()
+
+    def drawScore(score):
+        score_numb = score_numb_font.render(str(score),1,pygame.Color("red"))
+        screen.blit(score_msg, (SCREEN_WIDTH-score_msg_size[0]-60,10) )
+        screen.blit(score_numb,(SCREEN_WIDTH - 45,14))
+        
+    def drawGameTime(gameTime):
+        game_time = score_font.render("Time:",1,pygame.Color("green"))
+        game_time_numb = score_numb_font.render(str(gameTime/1000),1,pygame.Color("red"))
+        screen.blit(game_time,(30,10))
+        screen.blit(game_time_numb,(105,14))
+        
+    def exitScreen():
+        pass
 if __name__=="__name__":
         gameframe();                        
